e6d4c8554618d959da61b6deb0b0c7abc3491af0
BrunoAlz <bruno6821@gmail.com>
2022-07-21 00:38:49 -0300

    Criando a Interface SOZINHO

--- app/lib/campo_texto.dart
+++ app/lib/campo_texto.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_print, prefer_const_constructors
+// ignore_for_file: avoid_print, prefer_const_constructors, prefer_final_fields, prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
 
@@ -17,27 +17,36 @@ class _CampoTextoState extends State<CampoTexto> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Entrada de Dados'),
+        title: const Text('Ácool ou Gasolina'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(32),
+      body: Padding(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/logo.png',
+              height: 150,
+            ),
             // Cria um elemento Input
-            child: TextField(
+            TextField(
               // Escolhe o tipo de Teclado como númerico
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Digite o Valor'),
+              decoration: InputDecoration(
+                labelText: 'Preço Alccol, ex 3.89',
+              ),
               // enabled: true,
               // maxLength: 5,
               // maxLengthEnforcement: true,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.normal,
+              ),
               // obscureText: true, // Password
 
               // Recupera o valor no Change
               /*onChanged: (String valor) {
-                print(valor);
-              },*/
+                  print(valor);
+                },*/
 
               // Recupera o Valor no Submit
               onSubmitted: (String text) {
@@ -46,27 +55,56 @@ class _CampoTextoState extends State<CampoTexto> {
               // O Controle tem acesso aos dados digitados pelo usuário
               controller: _textEditingController,
             ),
-          ),
-          // Cria um botão
-          ElevatedButton(
-            // Adiciona estilos ao botão
-            style: ElevatedButton.styleFrom(
-              primary: Colors.blue, // background
-              onPrimary: Colors.white, // foreground
+            TextField(
+              // Escolhe o tipo de Teclado como númerico
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Preço Gasolina, ex 6.79',
+              ),
+              // enabled: true,
+              // maxLength: 5,
+              // maxLengthEnforcement: true,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.normal,
+              ),
+              // obscureText: true, // Password
+
+              // Recupera o valor no Change
+              /*onChanged: (String valor) {
+                  print(valor);
+                },*/
+
+              // Recupera o Valor no Submit
+              onSubmitted: (String text) {
+                // print(text);
+              },
+              // O Controle tem acesso aos dados digitados pelo usuário
+              controller: _textEditingController,
             ),
-            // Evento de click, cria uma função vazia e não faz nada
-            onPressed: () {
-              // Retorna o valor que foi armazenado pelo controlador - text são
-              // os dados do input
-              print('Valor digitado: ${_textEditingController.text}');
-            },
-            // cria o texto filho do botão
-            child: const Text(
-              'Salvar',
-              style: TextStyle(fontSize: 25),
-            ),
-          )
-        ],
+            // Cria um botão
+            ElevatedButton(
+              // Adiciona estilos ao botão
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue, // background
+                onPrimary: Colors.white, // foreground
+              ),
+              // Evento de click, cria uma função vazia e não faz nada
+              onPressed: () {
+                // Retorna o valor que foi armazenado pelo controlador - text são
+                // os dados do input
+                print('Valor digitado: ${_textEditingController.text}');
+              },
+              // cria o texto filho do botão
+              child: const Text(
+                'Salvar',
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
